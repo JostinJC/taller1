e5cb53aa2d856853a7c86197249d6de403ea279b
Jostin JC <jcedeno4234@utm.edu.ec>
2024-10-22 23:31:20 -0500

    carga1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    var message = document.getElementById("message");
+    message.style.display = "block";  // Muestra el mensaje oculto
+});
